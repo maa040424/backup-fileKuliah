4e218f9930f2431150c26a80a168ff9cfda5d744
Aqueit <alghifarimadam04@gmail.com>
2024-08-04 10:11:18 +0800

    day1 c++

--- belajar c++/day1/day1deklarasi&inisialisasi.cpp
+++ belajar c++/day1/day1deklarasi&inisialisasi.cpp
@@ -8,7 +8,7 @@ int main (){
 	double pi = 3.1415926535; // sama juga bilangan pecahan itu pakai titik untuk memisahkannya 
 	char grade = 'A'; 
 	bool isStudent = true; // boolean itu  bersifat true dan false atau 1 dan 0. kalo tidak true berarti false
-	string name = "Muhammad Adam Alghifari"; //string itu bisa pakai petik dua("___") bisa juga petik satu ('---')
+	string name = "Muhammad Adam Alghifari"; //string itu bisa pakai petik dua("___") bisa juga petik satu ('---')	
 	// nah saatnya bagian output 
 	
 	cout << "Nama :" << name << endl;
@@ -17,7 +17,7 @@ int main (){
 	cout << "Phi/Pi :" << pi << endl;
 	cout << "Grade :" << grade << endl;
 	cout << "Is Student :" << (isStudent ? "Yes" : "No") << endl; //pada bagian output boolean itu pakai kek gini ya 
-	
+	return 0;
 	
 	 
 }
