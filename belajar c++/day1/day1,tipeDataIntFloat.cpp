6a7460c7d4c9fe3f59fd8f408ea8d179efb9e80a
Aqueit <alghifarimadam04@gmail.com>
2024-08-04 10:04:39 +0800

    day1 c++

---
+++ belajar c++/day1/day1,tipeDataIntFloat.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	//deklarasi variabel 
+	int  a; //ini bilangan bulat
+	float  b; // ini bilangan pecahan
+	a = 10;
+	b = 20.5; // ingat untuk float itu pakai titik(.) bukan koma (,)
+	float hasil = a + b ; //hasil penjumlahan dari kedua variabel tersebut 
+	cout << "Hasil int a + float b adalah : " <<  hasil << endl; 
+}
