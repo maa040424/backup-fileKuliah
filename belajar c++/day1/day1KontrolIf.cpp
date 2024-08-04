970e17a84d4ddf296ad625d033b97aeb35192f3e
Aqueit <alghifarimadam04@gmail.com>
2024-08-04 10:16:06 +0800

    day1 c++

---
+++ belajar c++/day1/day1KontrolIf.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	int age = 20;
+	
+	if (age >= 16){
+		cout << "Kamu Sudah Dewasa Seharusnya kamu lebih giat belajar dan sukses di usia 25 agar orang tua mu bangga "<< endl;
+		
+	}
+	return 0;
+}
