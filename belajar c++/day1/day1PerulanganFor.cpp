a0f8d707a7da4513264c60ac2a8953a186011a2a
Aqueit <alghifarimadam04@gmail.com>
2024-08-04 10:41:46 +0800

    day1 c++

---
+++ belajar c++/day1/day1PerulanganFor.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	for (int i = 1; i <= 5; i++){
+		cout<< "Perulangan ke :"<< i << endl;
+	}
+	return 0;
+	/* 
+	penjelasan untuk perulangan for ini yang saya pahami adalah :
+	pada bagian (int i = 1 maka nilai awal variable i adalah 1)
+	terus kita buat operator atau apalah namanya dengan <= 5 maka i(1) itu kurang dari sama dengan 5 yang sudah di deklarasikan tadi
+	dan pada bagian i++ ini adalah increment ? lupa namanya. Nah dengan adanya i++ ini maka yang variable i nilainya 1 tadi akan ditambahkan
+	terus dengan angka 1 sampai akhirnya nilai i tadi melebihi angka 5 pada operator yang sudah di deklarasikan tadi maka perulangan for ini berhenti mencetak kodingan 
+	yang ada.
+	
+	*/
+	
+}
