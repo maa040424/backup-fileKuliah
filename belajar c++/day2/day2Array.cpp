13bf90cdd7cb6bfeacc7c4baa9d89d5ccc08410e
Aqueit <alghifarimadam04@gmail.com>
2024-08-05 14:55:21 +0800

    Day2

---
+++ belajar c++/day2/day2Array.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int number[5]{1,2,3,4,5};
+	
+	for(int i = 0; i <5; i++){
+		cout << "Index array ke :" << i <<number[i]<<endl;
+	}
+	return 0;
+	
+	/*
+	lebih mudah array dipahamin dari pada function wkwkwk sampai sekarang blum paham function
+	*/
+}
